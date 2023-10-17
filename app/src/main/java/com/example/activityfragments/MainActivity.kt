59b0ac5b28913736aeb25fa01abea8f049d62b5a
finnbassill <tun39698@temple.edu>
2023-10-17 12:20:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/activityfragments/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.activityfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container1, ColorSelectionFragment())
+            .commit()
+    }
+}
